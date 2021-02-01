b3c0c00c7852151e17a8f0367749f56261e5c34a
ArantxaGarciaMoreno <arantxagarciamoreno@gmail.com>
2021-02-01 06:40:09 -0400

    Entry Page specification done

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,9 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+def entry(request, title):
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "content": util.get_entry(title)
+    })
